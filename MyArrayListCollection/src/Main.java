641eb7cdb497c3be509534874c6f064764788db6
Gorbachev Alexey <smilebee777@mail.ru>
2024-02-11 21:44:44 +0300

    Main для проверки

--- MyArrayListCollection/src/Main.java
+++ MyArrayListCollection/src/Main.java
@@ -2,13 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
         MyArrayList<String> str = new MyArrayList<>();
-        str.add ("My");
-        str.add ("Name");
-        str.add ("is");
-        str.add ("A - Alexey");
+        str.add("My");
+        str.add("Name");
+        str.add("is");
+        str.add("A - Alexey");
+        str.add("check");
+        str.add("this");
+        str.add("MyArrayList");
+        System.out.println(str);
 
-        for (int i = 0; i < str.size(); i++) {
-            System.out.print(str.get(i) + " ");
-        }
+        MyArrayList<Integer> i = new MyArrayList<>();
+        i.add(15);
+        i.add(1);
+        i.add(0);
+        i.add(3);
+        i.add(-5);
+        MyList.sort(i);
+        System.out.println(i);
     }
 }
